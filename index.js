c176cddca5e0497b7f2f8b9385910f9b6f1c2f5a
Abdullah <abdullahiakande77@gmail.com>
2023-10-16 00:03:34 +0100

    Commiting my files

---
+++ index.js
@@ -0,0 +1,30 @@
+// alert("Working");
+
+
+
+// function initializeMailchimp() {
+//     const InputField = document.getElementById("Input_field");
+//     const btn = document.getElementById("btn")
+//     const form = document.getElementById("form")
+
+//     form.addEventListener("submit", function(event) {
+//         event.preventDefault();
+
+//         const email = InputField.ariaValueMax;
+        
+
+//         mailchimp_subscribe({
+//             url: 'https://xxabdullahixx.us1.list-manage.com/subscribe/post?u=1ead9dc3bd'
+//         })
+//     })
+// }
+
+
+
+// InputField.addEventListener("input", function(event) {
+//     const typedValue = event.target.value;
+//     console.log(typedValue)
+// })
+
+// api key = d8252efcb0907b69d5210d9a851acb26-us21
+// listid = 1ead9dc3bd
